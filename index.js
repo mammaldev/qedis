2059de3d35ca6d7b08a107e4b129f9e80fbf281a
Nick Cartwright <nick@aslant.io>
2014-05-27 20:52:28 +0100

    use common promise fail handler

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ function Qedis(redisClient) {
 
 Qedis.prototype.exists = function(cacheKey) {
   return Q.ninvoke(this.redisClient, 'exists', cacheKey)
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.fetch = function(cacheKey) {
@@ -24,68 +21,49 @@ Qedis.prototype.fetch = function(cacheKey) {
       })
     );
   })
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.get = function(cacheKey) {
   return Q.ninvoke(this.redisClient, 'get', cacheKey)
   .then(JSON.parse.bind(JSON))
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.set = function(cacheKey, value) {
   return Q.ninvoke(this.redisClient, 'set', cacheKey, JSON.stringify(value))
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.blpop = function() {
   return Q.ninvoke.apply(Q, [this.redisClient, 'blpop'].concat([].slice.call(arguments)))
   .then(JSON.parse.bind(JSON))
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.lrange = function(start, end) {
   return Q.ninvoke(this.redisClient, 'lrange', start, end)
   .then(JSON.parse.bind(JSON))
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.lpop = function(list) {
   return Q.ninvoke(this.redisClient, 'lpop', list)
   .then(JSON.parse.bind(JSON))
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.lpush = function(list, obj) {
   return Q.ninvoke(this.redisClient, 'lpush', list, JSON.stringify(obj))
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
 
 Qedis.prototype.rpush = function(list, obj) {
   return Q.ninvoke(this.redisClient, 'rpush', list, JSON.stringify(obj))
-  .fail(function(err) {
-    console.log(err);
-    throw err;
-  });
+  .fail(failureHandler)
 };
+
+function failureHandler(err) {
+  console.log(err.stack);
+  throw err;
+}
